2db83accc7f29f8802bce058b4d7e6c5862b8d66
Tony Lin <tony.lin@ardomusnet.com>
2017-09-14 23:53:27 +0800

    Add type

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,7 @@
-(function($) {
-    "use strict"; // Start of use strict
-
-
-})(jQuery); // End of use strict
+var typed = new Typed('.typing', {
+  strings: ["an Android app programmer", "a backend engineer", "a frontend engineer", "a fullstack enginner"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+  showCursor: false
+});
